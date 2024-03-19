49f6222109e99155f2e0dde742e96442c1de918e
Gustavo Oli <69ggustavoo69@gmail.com>
2024-03-19 19:45:04 -0300

    update main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,37 @@
 import '../App.css';
 import React from 'react';
+import styled from 'styled-components'
 
 
 
-
-
+const DivSection = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: auto;
+`;
+const TestStylePerfil = styled.h1 `
+font-size: 2rem;
+color: black;
+`;
 
 function Main(){
     return(
         <>
         <div className='MainPage'>
-
+            <div className='SectionPerfil'> 
+                <div className='SectionPerfil-Perfil'></div> 
+                
+            </div>
+            <DivSection>
+                <TestStylePerfil>
+                    Gustavo Oliveira
+                </TestStylePerfil>
+            </DivSection>
+            <DivSection>
+                <div className='IconGithub'></div>
+            </DivSection>
+            
 
 
 
