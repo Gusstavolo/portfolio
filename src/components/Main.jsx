102c9aa6099ed2f9250c0e90c9e979393c6f8eb3
Gustavo Oli <69ggustavoo69@gmail.com>
2024-03-19 19:22:01 -0300

    config page

---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+import '../App.css';
+import React from 'react';
+
+
+
+
+
+
+function Main(){
+    return(
+        <>
+        <div className='MainPage'>
+
+
+
+
+        </div>
+      
+        </>
+    )
+    
+}
+export default Main;
