a936659fa6f978c161d196fabc9375f4972bb385
Gustavo Oli <69ggustavoo69@gmail.com>
2024-03-21 18:01:44 -0300

    teest txt

--- src/components/MeFor3d.jsx
+++ src/components/MeFor3d.jsx
@@ -3,10 +3,10 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function Me3dT(props) {
-  const { nodes, materials } = useGLTF('./models/3DMODELME.glb')
+  const { nodes, materials } = useGLTF('./models/me.glb')
   return (
     <group {...props} dispose={null} position={[0, -0.34, 0]} >
-      <mesh geometry={nodes.Mesh_0.geometry} material={materials.Material_0} scale={1.911} />
+      <mesh geometry={nodes.Mesh_0.geometry} rotation={[-Math.PI / 2, 0, 0]} material={materials.Material_0} scale={1.911} />
     </group>
   )
 }
